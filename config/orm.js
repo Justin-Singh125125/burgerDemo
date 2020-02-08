a0ac6e7edd23a010f4b0afcd5253cb3d1c96f3af
Justin Singh <justin.singh125125@gmail.com>
2020-02-07 17:37:54 -0800

    demo

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,13 @@
 const connection = require("./connection");
 
-// function objToSql(ob) {
-//     // column1=value, column2=value2,...
-//     var arr = [];
+function convertObjToSQL(obj) {
+    let array = [];
 
-//     for (var key in ob) {
-//         arr.push(key + "=" + ob[key]);
-//     }
-
-//     return arr.toString();
-// }
-
+    for (keys in obj) {
+        array.push(`${keys}=${obj[keys]}`);
+    }
+    return array.toString();
+}
 function printQuestionMarks(num) {
     let array = [];
 
@@ -27,21 +24,22 @@ const orm = {
 
     },
     createOne: function (tableName, columns, values, callbackFunction) {
-
+        // const queryString = `INSERT INTO burgers ("burger_name", "devoured") VALUES (?,?,?)`
         const queryString = `INSERT INTO ${tableName} (${columns.toString()}) VALUES (${printQuestionMarks(values.length)})`
 
         connection.query(queryString, values, function (err, results) {
             callbackFunction(results);
         })
     },
-    updateOne: function () {
+    updateOne: function (tableName, values, condition, callbackFunction) {
         // const queryString = `UPDATE burgers SET devoured=true WHERE id=3`
-
+        const queryString = `UPDATE ${tableName} SET `
         connection.query(queryString, function (err, result) {
 
         })
     },
 }
+
 
 module.exports = orm;
 
